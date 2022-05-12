85359cc70a7da6dc731b7e8892ae4ebc798de6ed
ranaahammed012 <98573219+ranaahammed012@users.noreply.github.com>
2022-05-12 14:23:00 +0600

    root api created

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(cors());
 
 const port = 5000;
 
+app.get("/", (req, res) => {
+  res.send("Hello from DataBase It's working")
+})
+
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
