8900b914ea60ef5bc57c7472754ddb7b482a13bf
ranaahammed012 <98573219+ranaahammed012@users.noreply.github.com>
2022-08-31 18:53:22 +0600

    upto 50/2 done

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-const express = require('express')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hiastzp.mongodb.net/?retryWrites=true&w=majority`;
+
+
 const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
 const port = 5000;
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://rana:<password>@cluster0.drrfzyp.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+  const productsCollection = client.db("emaJohnStore").collection("products");
+
+  app.post("/addProduct", (req, res) => {
+    const products = req.body;
+    productsCollection.insertMany(products)
+    .then(result => {
+      console.log(result.acknowledged);
+      res.send(result.acknowledged)
+    })
+  })
+   
 });
 
-
 app.listen(port)
+
+
+
+
+
